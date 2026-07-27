0dfdb871084e7b7b09609ba43c018d77c7fb976a
원민관 <a4ueo@naver.com>
2026-07-27 21:44:17 +0900

    refactor: blog-list post-list 폴더 중첩 제거

--- components/blog-list/post-list.tsx
+++ components/blog-list/post-list.tsx
@@ -1,8 +1,8 @@
 "use client"
 
 import { AsyncBoundary } from "@/components/shared/async-boundary"
-import { PostListContent } from "@/components/blog-list/post-list/post-list-content"
-import { PostListSkeleton } from "@/components/blog-list/post-list/post-list-skeleton"
+import { PostListContent } from "@/components/blog-list/post-list-content"
+import { PostListSkeleton } from "@/components/blog-list/post-list-skeleton"
 
 export function PostList() {
     return (
