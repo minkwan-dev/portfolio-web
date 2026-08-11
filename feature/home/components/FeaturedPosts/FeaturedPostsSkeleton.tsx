c164c6ef1ab3a8ecab12475b1f4ff3345cbb3665
minkwan-dev <a4ueo@naver.com>
2026-08-11 12:20:48 +0900

    refactor: FeaturedPostsSkeleton의 스켈레톤 개수 상수 제거

--- feature/home/components/FeaturedPosts/FeaturedPostsSkeleton.tsx
+++ feature/home/components/FeaturedPosts/FeaturedPostsSkeleton.tsx
@@ -1,19 +1,16 @@
 import { FeaturedPostsHeader } from "@/feature/home/components/featuredPosts/FeaturedPostsHeader"
 
-const SKELETON_ROWS = 5
-const SKELETON_COLS = 3
-
 export function FeaturedPostsSkeleton() {
     return (
     <section className="mx-auto flex w-full max-w-5xl flex-col px-6 pb-16 pt-6">
         <div className="flex flex-col gap-10 border-t border-gray-300 pt-5">
             <FeaturedPostsHeader />
-            {Array.from({ length: SKELETON_ROWS }).map((_, rowIndex) => (
+            {Array.from({ length: 5 }).map((_, rowIndex) => (
             <div
                 key={rowIndex}
                 className="grid grid-cols-1 gap-x-6 gap-y-5 sm:grid-cols-2 lg:grid-cols-3"
             >
-                {Array.from({ length: SKELETON_COLS }).map((__, colIndex) => (
+                {Array.from({ length: 3 }).map((__, colIndex) => (
                 <div
                     key={colIndex}
                     className="h-[260px] animate-pulse rounded-2xl border border-gray-200 bg-gray-100"
